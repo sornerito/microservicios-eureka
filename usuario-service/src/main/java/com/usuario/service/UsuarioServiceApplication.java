8c06e45cec0dfde2e8298c556fde6662d7b21aab
sornerito <samueleonardo.escuela@gmail.com>
2024-04-21 18:43:13 -0500

    Eureka

--- usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
@@ -2,10 +2,13 @@ package com.usuario.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
+
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
